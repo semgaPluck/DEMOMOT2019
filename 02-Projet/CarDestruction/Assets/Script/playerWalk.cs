be22c05cdca48220be0206a625895dbfc0faa353
semgaPluck <diogonavegapinto@gmail.com>
2019-06-04 18:52:59 +0200

    création du projet unity, création du terrain,du joueur et du script de mouvement

---
+++ 02-Projet/CarDestruction/Assets/Script/playerWalk.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerWalk : MonoBehaviour
+{
+
+    [SerializeField]
+    Rigidbody rbPlayer;
+
+    [SerializeField]
+    float speed;
+
+
+    private float _X = 0;
+    private float _Z = 0;
+
+    public playerWalk(float X, float Z)
+    {
+        float _X = X;
+        float _Z = Z;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _X = Input.GetAxisRaw("Horizontal");
+        _Z = Input.GetAxisRaw("Vertical");
+
+        Vector2 velocity = new Vector2(_X,_Z);
+
+        this.walk(velocity);
+
+
+    }
+
+    void walk(Vector2 velocity)
+    {
+        rbPlayer.AddForce(velocity, speed)
+    }
+}
